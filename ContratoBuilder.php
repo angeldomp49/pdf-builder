531f1cf4fc26892ac190056073aa4f4d88c93ffa
Makech Tecnology <contacto@makechtecnology.com>
2021-11-20 17:54:58 -0600

    first commit

---
+++ ContratoBuilder.php
@@ -0,0 +1,24 @@
+<?php
+namespace App\PixelSiete;
+
+use Spipu\Html2Pdf\Html2Pdf;
+
+class ContratoBuilder extends Html2Pdf{
+
+    public static function default(){
+        return new self( 'P','A4','es','true','UTF-8' );
+    }
+
+    public function generate( $html, $path){
+        $this->content($html);
+        $this->store($path);
+    }
+
+    protected function content($html){
+        $this->writeHTML($html);
+    }
+
+    protected function store($path){
+        $this->output($path, 'F');
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+una simple capa de abstracción para el html2pdf
+se espera que en un futuro se mejoren las funciones que esta libreria ofrece
